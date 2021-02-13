00d594bea852a745e636e4d764f3bffd5dc9e45a
LukeS26 <LukeS062022@gmail.com>
2021-02-13 15:57:42 -0500

    "fixed" hash

--- public/register.js
+++ public/register.js
@@ -54,16 +54,5 @@ function checkForm() {
 }
 
 function stringToHash(string) { 
-                  
-    var hash = 0; 
-      
-    if (string.length == 0) return hash; 
-      
-    for (i = 0; i < string.length; i++) { 
-        char = string.charCodeAt(i); 
-        hash = ((hash << 5) - hash) + char; 
-        hash = hash & hash; 
-    } 
-      
-    return hash; 
+    return string; 
 } 

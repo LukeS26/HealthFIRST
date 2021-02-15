beb61e6cf7ded5fd617721e4abf0ade4f9eddbc5
LukeS26 <LukeS062022@gmail.com>
2021-02-15 17:58:47 -0500

    fix

--- public/post.js
+++ public/post.js
@@ -1,3 +1,12 @@
+function getUrlVars() {
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+        vars[key] = value;
+    });
+    return vars;
+}
+
+
 let id = getUrlVars()["id"];
 
 console.log(id);

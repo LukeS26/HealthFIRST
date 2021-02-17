bcb7efd7390ea98a771f600b6a160f69ba671428
LukeS26 <LukeS062022@gmail.com>
2021-02-17 11:43:50 -0500

    fixed comment

--- public/home.js
+++ public/home.js
@@ -46,12 +46,12 @@ function toggleCommenter(id) {
 
 	if (open[id]) {
 		img.style.transform = "rotate(0deg)";
-		div[4].style.display = "none";
+		div[1].style.display = "none";
 		open[id] = false;
 	} else {
 		img.style.transform = "rotate(-180deg)";
-		div[4].style.display = "block";
-		div[4].focus();
+		div[1].style.display = "block";
+		div[1].focus();
 		open[id] = true;
 	}
 }

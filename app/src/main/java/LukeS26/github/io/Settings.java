e08a55e1723d9fb7bcec2f8cda34adf446b47525
Infinium1 <jeremychernin@gmail.com>
2021-02-17 10:38:40 -0500

    Updated admin password, cleaning

--- app/src/main/java/LukeS26/github/io/Settings.java
+++ app/src/main/java/LukeS26/github/io/Settings.java
@@ -4,7 +4,7 @@ public class Settings {
    public static final String WEBSITE_URL = "157.230.233.218";
    public static final int MONGO_PORT = 27017;
    public static final String MONGO_ADMIN_USERNAME = "admin";
-   public static final String MONGO_ADMIN_PASSWORD = "Demonator9Robotics";
+   public static final String MONGO_ADMIN_PASSWORD = ")(8_wS@~8ftNy5$6G$g*:#FH$]2t`p$$<yHn6U)uu.!Fy)Q:!`";
    public static final String MONGO_URI = "mongodb://" + MONGO_ADMIN_USERNAME + ":" + MONGO_ADMIN_PASSWORD + "@157.230.233.218:27017/?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false";
 
    public static final String MONGO_DATABASE_NAME = "InnovationApp";

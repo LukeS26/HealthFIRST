8667e36d974c9a52a3b9f4aa79da5f5f32d998a9
jchernin4 <jeremychernin@gmail.com>
2021-02-23 15:50:38 -0500

    temp

--- app/src/main/java/LukeS26/github/io/HttpServer.java
+++ app/src/main/java/LukeS26/github/io/HttpServer.java
@@ -188,7 +188,7 @@ public class HttpServer {
          * Create a post
          */
         app.post("/api/posts", ctx -> {
-            ctx.header("Access-Control-Allow-Origin", "*");`
+            ctx.header("Access-Control-Allow-Origin", "*");
             // ctx.res.setHeader("Access-Control-Allow-Origin", "http://" + Settings.WEBSITE_URL);
 
             Document doc = null;
@@ -378,7 +378,7 @@ public class HttpServer {
          */
         app.post("/api/account/login", ctx -> {
             if (ctx.headerMap().containsKey("Origin") && ctx.header("Origin").contains(Settings.WEBSITE_URL)) {
-                ctx.res.setHeader("Access-Control-Allow-Origin", "http://" + Settings.WEBSITE_URL);
+                ctx.res.setHeader("Access-Control-Allow-Origin", "http://157.230.233.218");
             }
 
             Document doc = null;

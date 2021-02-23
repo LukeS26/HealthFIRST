fce2d8e8731189235f367fccb7815e3317b75689
jchernin4 <jeremychernin@gmail.com>
2021-02-23 15:44:44 -0500

    update

--- app/src/main/java/LukeS26/github/io/HttpServer.java
+++ app/src/main/java/LukeS26/github/io/HttpServer.java
@@ -188,7 +188,7 @@ public class HttpServer {
          * Create a post
          */
         app.post("/api/posts", ctx -> {
-            ctx.header("Access-Control-Allow-Origin", "*");
+            ctx.header("Access-Control-Allow-Origin", "*");`
             // ctx.res.setHeader("Access-Control-Allow-Origin", "http://" + Settings.WEBSITE_URL);
 
             Document doc = null;
@@ -377,7 +377,9 @@ public class HttpServer {
          * Getting an account token + verifying username+pass
          */
         app.post("/api/account/login", ctx -> {
-            ctx.res.setHeader("Access-Control-Allow-Origin", "http://" + Settings.WEBSITE_URL);
+            if (ctx.headerMap().containsKey("Origin") && ctx.header("Origin").contains(Settings.WEBSITE_URL)) {
+                ctx.res.setHeader("Access-Control-Allow-Origin", "http://" + Settings.WEBSITE_URL);
+            }
 
             Document doc = null;
             try {

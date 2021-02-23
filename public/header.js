63a234cb71497d63951bc80e9929fde3267b01c4
kjelstromjr <jeffreykjelstrom@gmail.com>
2021-02-23 11:49:18 -0500

    zxfvkzbsdh bzx zbkz

--- public/header.js
+++ public/header.js
@@ -58,12 +58,12 @@ fetch("http://157.230.233.218:8080/api/posts", {
 	method: "POST",
 	body: JSON.stringify(data),
 	headers: {
-		'Content-type': 'application/json; charset=UTF-8'
+		'Content-type': 'application/json; charset=UTF-8',
+		'Authorization': currToken
 	},
 	mode: "cors",
 	headers: {
-		'Origin': 'http://157.230.233.218:8080/api/posts',
-		'Authorization': currToken
+		'Origin': 'http://157.230.233.218:8080/api/posts'
 	} 
 })
 .then(res => {

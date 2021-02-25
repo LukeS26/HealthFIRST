b48cfe304710a0875297ed1402489b06902bf4c8
kjelstromjr <jeffreykjelstrom@gmail.com>
2021-02-25 14:42:05 -0500

    sdfaksdfaehaecaesc

--- public/header.js
+++ public/header.js
@@ -62,7 +62,8 @@ fetch("http://157.230.233.218:8080/api/posts", {
 		'Content-type': 'application/json; charset=UTF-8',
 		'Authorization': currToken
 	},
-	mode: "cors"
+	mode: "cors",
+	credentials: "same-origin"
 })
 .then(res => {
 	let code = res.status;

96eff6760865976ff4460bae3c941fac5fe280c3
kjelstromjr <jeffreykjelstrom@gmail.com>
2021-02-25 10:28:08 -0500

    iuhomiuhomsiuhfmosd

--- public/header.js
+++ public/header.js
@@ -59,12 +59,14 @@ fetch("http://157.230.233.218:8080/api/posts", {
 	method: "POST",
 	body: JSON.stringify(data),
 	headers: {
-		'Content-type': 'application/json; charset=UTF-8',
-		'Authorization': currToken
+		'Content-type': 'application/json; charset=UTF-8'
 	},
 	mode: "cors",
 	headers: {
 		"Origin": "http://157.230.233.218"
+	},
+	headers: {
+		"Authorization": currToken
 	}
 })
 .then(res => {

3f0647713049a87d82748a4233f06843821ae145
kjelstromjr <jeffreykjelstrom@gmail.com>
2021-02-26 17:46:36 -0500

    asudfhoaisudhfoaiusdhfmoausxidfhm

--- public/profiles.js
+++ public/profiles.js
@@ -1,5 +1,27 @@
-function Profile(name, picture, id) {
-	this.name = name;
-	this.pic = picture;
-	this.id = id;
+let profileOut = document.getElementById("profileOut");
+
+let url = "http://157.230.233.218:8080/api/account/" + getCookie("username");
+fetch(fetchUrl)
+		.then(res => res.json())
+		.then(json => {
+			profileOut.innerHTML = json;
+		})
+		.catch(function (error) {
+			console.log(error);
+
+			return null;
+		});
+
+function getCookie(name) {
+	let cookieArr = document.cookie.split(";");
+
+	for (let i = 0; i < cookieArr.length; i++) {
+		let cookiePair = cookieArr[i].split("=");
+
+		if (name === cookiePair[0].trim()) {
+			return cookiePair[1];
+		}
+	}
+
+	return null;
 }

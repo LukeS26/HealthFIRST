776bb554c58448ca3ed64608dea63b27fee8ba80
kjelstromjr <jeffreykjelstrom@gmail.com>
2021-03-09 12:55:49 -0500

    sufiouahmsdoiufahosudfas

--- public/header.js
+++ public/header.js
@@ -68,6 +68,6 @@ if( navigator.userAgent.match(/Android/i)
  || navigator.userAgent.match(/iPad/i)
  || navigator.userAgent.match(/iPod/i)
  || navigator.userAgent.match(/BlackBerry/i)
- || navigator.userAgent.match(/Windows Phone/i) {
+ || navigator.userAgent.match(/Windows Phone/i)) {
 	alert("You're using Mobile Device!!")
 }

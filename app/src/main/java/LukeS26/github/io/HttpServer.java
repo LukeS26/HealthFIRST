51809ed47a7666f6b2dafb96f8ad589b5edef235
jchernin4 <jeremychernin@gmail.com>
2021-03-13 15:49:59 -0500

    Removing regex check for links

--- app/src/main/java/LukeS26/github/io/HttpServer.java
+++ app/src/main/java/LukeS26/github/io/HttpServer.java
@@ -320,16 +320,18 @@ public class HttpServer {
                 }
 
                 // Checking if using a link to an image
-                if (e.getKey().equals("profile_picture_link")) {
+                // TODO: Certain links don't work, removing for now
+                /*if (e.getKey().equals("profile_picture_link")) {
                     // Checks for http:// or https://, has to be a valid site and have a file extension at the end (https://site.com/image.png)
                     // Since its checking an unlimited amount of characters for the site name, it could be www.site.com or just site.com, it doesn't matter
+                    
                     Pattern p = Pattern.compile("^http[s]{0,1}:\\/\\/.*\\/.*\\.[a-zA-Z]{3,4}");
                     Matcher m = p.matcher((String) e.getValue());
                     if (!m.find()) {
                         ctx.status(HttpStatus.BAD_REQUEST_400);
                         return;
                     }
-                }
+                }*/
 
 
                 // TODO: Check if setting profile pic link to something besides a link, etc.

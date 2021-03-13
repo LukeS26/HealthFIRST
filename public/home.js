f040647388d7fc15be838c25997116507c69b9cf
LukeS26 <LukeS062022@gmail.com>
2021-03-13 13:27:07 -0500

    test

--- public/home.js
+++ public/home.js
@@ -64,7 +64,7 @@ loadPage(0);
 
 window.onscroll = function(ev) {
     if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
-		page++;
+		page+=2;
 		loadPage(page);
     }
 };

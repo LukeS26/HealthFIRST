f717339a25cd20eb1f32f71ca25135114d746b9d
LukeS26 <LukeS062022@gmail.com>
2021-03-16 21:28:28 -0400

    test

--- public/post.js
+++ public/post.js
@@ -89,7 +89,7 @@ function formatReplies(replyArr) {
 	if (Array.isArray(replyArr)) {
 		for (let i = 0; i < replyArr.length; i++) {
 			if (!Array.isArray(replyArr[i])) {
-				load(replyArr[i]["body"], num);
+				load(replyArr[i]["body"], num, replyArr[i]["author"]);
 			}
 			formatReplies(replyArr[i]);
 			num--;
@@ -97,8 +97,8 @@ function formatReplies(replyArr) {
 	}
 }
 
-function load(reply, number) {
-	let comment = `<div name="${number}" style="left: ${(30 * number) + 30}px; position: relative;" > <p> ${reply} </p> </div> `
+function load(reply, number, user) {
+	let comment = `<div name="${number}" style="left: ${(30 * number) + 30}px; position: relative;" > <a href="/user?${user}> ${user} </a> <p> DATE HERE </p> <br> <p> ${reply} </p> </div> `
 	
 	let shell = document.getElementById("comments");
 

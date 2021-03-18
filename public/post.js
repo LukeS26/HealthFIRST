71ed17a8daf919270fead3f42f479cc4024a656c
LukeS26 <LukeS062022@gmail.com>
2021-03-18 14:45:46 -0400

    test

--- public/post.js
+++ public/post.js
@@ -116,6 +116,8 @@ function load(reply, number, user, cid) {
 function openCommentField(el, cid) {
 	if(cid == null) {
 		//REPLYING TO POST
+		let commentField = `<div> <input placeholder="Comment" id="inputField${cid}"> <button onClick="makeCommentOnPost(this.parentElement.childNodes[1].value); this.parentElement.remove()"> Submit </button> <button onClick="this.parentElement.remove()"> Cancel </button> </div>`
+		el.parentElement.parentElement.innerHTML += commentField;
 	} else {
 		//REPLYING TO COMMENT
 		let commentField = `<div> <input placeholder="Comment" id="inputField${cid}"> <button onClick="makeComment('${cid}', this.parentElement.childNodes[1].value); this.parentElement.remove()"> Submit </button> <button onClick="this.parentElement.remove()"> Cancel </button> </div>`

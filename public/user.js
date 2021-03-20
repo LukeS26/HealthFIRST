58c91a16b5c64c8cc04b6f33c002e0203cd3e4ce
LukeS26 <LukeS062022@gmail.com>
2021-03-20 13:16:38 -0400

    formatted userpage

--- public/user.js
+++ public/user.js
@@ -1,7 +1,7 @@
 function getProfile(name) {
 	fetch(`http://157.230.233.218:8080/api/account/${name}`)
 		.then(res => res.json())
-		.then(function (json) { console.log(json) });
+		.then(function (json) { loadProfile(json) });
 }
 
 let user = window.location.href.split("?")[1]
@@ -14,10 +14,10 @@ function formatText(text) {
 	text = text.join("&nbsp;")
 	text = text.split("**");
 
-	for(let i = 0; i < text.length; i++) {
-		if(i % 2 != 0) {
-	  text[i] = "<b>" + text[i] + "</b>"
-	}
+	for (let i = 0; i < text.length; i++) {
+		if (i % 2 != 0) {
+			text[i] = "<b>" + text[i] + "</b>"
+		}
 	}
 
 	text = text.join("").split("*");
@@ -27,6 +27,19 @@ function formatText(text) {
 			text[i] = "<i>" + text[i] + "</i>"
 		}
 	}
-	
+
 	return text.join("");
+}
+
+
+function loadProfile(json) {
+	if (json.profile_picture_link !== null && json.profile_picture_link !== "null") {
+		document.getElementById("userPhoto").src = profile_picture_link;
+	}
+
+	document.getElementById("username") = json.username;
+	
+	if(json.biography != null) {
+		document.getElementById("userBio") = formatText(json.biography);
+	}
 }

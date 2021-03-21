ebf02b32758331fb49a71a9381f3d4dc71c2abd7
kjelstromjr <jeffreykjelstrom@gmail.com>
2021-03-21 15:55:46 -0400

    asdufhasoiudfas

--- public/home.js
+++ public/home.js
@@ -58,10 +58,9 @@ function displayPost(post, id) {
 		userImg = `<div class="profileImage" style="width: 30px; height: 30px; overflow: hidden; display: inline-block; position: relative; top: 8px;"><img src="${cookie}" height="30px" width="30px"></div>`;
 	} else {
 		let url;
-		console.log(post.author);
 		fetch(`http://157.230.233.218:8080/api/account/${post.author}`)
 			.then(res => res.json())
-			.then(function (json) { url = json.profile_picture_link });
+			.then(function (json) { url = json.profile_picture_link; console.log(json) });
 			userImg = `<div class="profileImage" style="width: 30px; height: 30px; overflow: hidden; display: inline-block; position: relative; top: 8px;"><img src="${url}" height="30px" width="30px"></div>`;
 	}
 

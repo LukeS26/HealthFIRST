3c17d1a0a4a53a3a34272f4b8110797dae37ec86
kjelstromjr <jeffreykjelstrom@gmail.com>
2021-03-21 15:15:27 -0400

    asudfoiusachdmofiuasd

--- public/home.js
+++ public/home.js
@@ -49,7 +49,7 @@ function displayPost(post, id) {
 
 	let userImg = "";
 	if (post.author === getCookie("username")) {
-		userImg = `<div class="profileImage" style="width: 30px; height: 30px; overflow: hidden; display: inline-block; position: relative; top: 10px;"><img src="${getCookie("imgUrl")}" height="30px" width="30px"></div>`
+		userImg = `<div class="profileImage" style="width: 30px; height: 30px; overflow: hidden; display: inline-block; position: relative; top: 8px;"><img src="${getCookie("imgUrl")}" height="30px" width="30px"></div>`
 	}
 
 	html += `<div tabindex="0" id="postOpen" onclick="loadPost('${id}')"><h1 class='postTitle'>${post.title}</h1>`;

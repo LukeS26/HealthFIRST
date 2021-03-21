b705ddb410e95df828387f70c96c59280bac7729
kjelstromjr <jeffreykjelstrom@gmail.com>
2021-03-21 16:18:04 -0400

    alksdfhlacsudhfmoasi

--- public/home.js
+++ public/home.js
@@ -57,13 +57,12 @@ function displayPost(post, id) {
 		}
 		userImg = `<div class="profileImage" style="width: 30px; height: 30px; overflow: hidden; display: inline-block; position: relative; top: 8px;"><img src="${cookie}" height="30px" width="30px"></div>`;
 	} else {
-		let url = "";
 		fetch(`http://157.230.233.218:8080/api/account/${post.author}`)
 			.then(res => res.json())
-			.then(function (json) { url = json.profile_picture_link; console.log(json) });
-			while(url === "") {}
-			userImg = `<div class="profileImage" style="width: 30px; height: 30px; overflow: hidden; display: inline-block; position: relative; top: 8px;"><img src="${url}" height="30px" width="30px"></div>`;
-	}
+			.then(function (json) {
+				userImg = `<div class="profileImage" style="width: 30px; height: 30px; overflow: hidden; display: inline-block; position: relative; top: 8px;"><img src="${json.profile_picure_link}" height="30px" width="30px"></div>`;
+			});
+			}
 
 	html += `<div tabindex="0" id="postOpen" onclick="loadPost('${id}')"><h1 class='postTitle'>${post.title}</h1>`;
 	html += `<a class='postAuthor' href='/user.html?${post.author}' >${userImg}<span style="padding-left: 5px">${post.author}<span></a>`;

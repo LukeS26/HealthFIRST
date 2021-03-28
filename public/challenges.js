7819c102b400af032ded7bcbc642697b4d8007c4
LukeS26 <LukeS062022@gmail.com>
2021-03-27 23:55:56 -0400

    test

--- public/challenges.js
+++ public/challenges.js
@@ -1,6 +1,8 @@
 function getChallenges(page) {
 	fetch(`http://157.230.233.218:8080/api/challenges/feed?page=${page}`)
 	.then(res => res.json()).then(function(json) {
+		console.log(json);
+		console.log(json["feed"])
 		for(let i = 0; i < json["feed"]; i++) {
 			console.log(json["feed"]);
 		}

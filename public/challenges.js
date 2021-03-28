0e8e29528f069f664a2f0780ebeae73867378525
LukeS26 <LukeS062022@gmail.com>
2021-03-28 00:16:18 -0400

    testing post shells

--- public/challenges.js
+++ public/challenges.js
@@ -2,7 +2,23 @@ function getChallenges(page) {
 	fetch(`http://157.230.233.218:8080/api/challenges/feed?page=${page}`)
 	.then(res => res.json()).then(function(json) {
 		for(let i = 0; i < json["feed"].length; i++) {
-			console.log(json["feed"]);
+			displayChallenge(json["feed"][i]["title"], json["feed"][i]["body"], json["feed"][i]["end_date"]);
 		}
 	});
 }
+
+function displayChallenge(title, body, date) {
+	let dateFormatted = new Date(date).toISOString;
+	let shell = document.getElementById("challenges");
+	let html = "";
+	
+	html += `<h3> ${title} </h3>`;
+	html += `<p> End: ${dateFormatted} </p>`
+	html += `<p> ${body} </p>`;
+
+	shell += `<div class="postContainer"> ${html} </div>`
+}
+
+let page = 0;
+
+getChallenges(page);

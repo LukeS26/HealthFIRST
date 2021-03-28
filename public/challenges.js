fda4bf7e4e236aa84ad1fee23277028afdb9e5ea
LukeS26 <LukeS062022@gmail.com>
2021-03-27 23:58:28 -0400

    fixed loop

--- public/challenges.js
+++ public/challenges.js
@@ -1,9 +1,7 @@
 function getChallenges(page) {
 	fetch(`http://157.230.233.218:8080/api/challenges/feed?page=${page}`)
 	.then(res => res.json()).then(function(json) {
-		console.log(json);
-		console.log(json["feed"])
-		for(let i = 0; i < json["feed"]; i++) {
+		for(let i = 0; i < json["feed"].length; i++) {
 			console.log(json["feed"]);
 		}
 	});

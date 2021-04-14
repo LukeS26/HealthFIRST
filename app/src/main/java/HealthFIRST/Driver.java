5e0a16a204f8abb955d5770a1e3455794c640e05
Infinium1 <jeremychernin@gmail.com>
2021-04-13 20:33:34 -0400

    Fixing, Intellij broke when recloning on server

--- app/src/main/java/HealthFIRST/Driver.java
+++
@@ -1,9 +0,0 @@
-package HealthFIRST;
-
-public class Driver {
-    public static void main(String[] args) {
-        HttpServer server = HttpServer.getInstance();
-
-        server.start();
-    }
-}
